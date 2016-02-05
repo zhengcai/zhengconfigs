8105bc8be9be0b7d950992dac8aa79c589518b84
Zheng Cai <zheng@cohesity.com>
2016-02-05 15:48:22 -0800

    Additional settins for windows emacs

--- .emacs.el
+++ .emacs.el
@@ -101,6 +101,23 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 ;; meta-Enter on the IBM thinkpad in Arora 6 generates meta-return
 ;; when in an x window, and meta-linefeed when a terminal
 (global-set-key '[(meta return)]  'dabbrev-expand)
